ace4380756d9a32773e9cbec3b0ffc519a49663c
fred04q4 <93513730+fred04q4@users.noreply.github.com>
2022-01-06 15:12:51 +0100

    færdig side

---
+++ js/show.js
@@ -0,0 +1,9 @@
+const button = document.querySelector(".button");
+const text1 = document.querySelector(".text1");
+const text2 = document.querySelector(".text2");
+
+button.addEventListener("click", () => {
+  button.classList.toggle("knap-3");
+  text1.classList.toggle(".hide");
+  text2.classList.toggle(".hide");
+});
